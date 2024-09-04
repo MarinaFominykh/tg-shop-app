07353d3547fd46d07dac8d79154e575d24aa5231
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2024-09-04 16:21:33 +0500

    хук для получения объекта Telegram

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { useEffect } from "react";
-
+import { useTelegram } from "./hooks/useTelegram";
+import { Header } from "./components/Header/Header";
 function App() {
-  const tg = window.Telegram.WebApp;
+  const { tg } = useTelegram();
 
   useEffect(() => {
     tg.ready();
   }, []);
 
-  return <main></main>;
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
 }
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,13 @@
 import "./Header.css";
 import { Button } from "../Button/Button";
+import { useTelegram } from "../../hooks/useTelegram";
 export const Header = () => {
-  const tg = window.Telegram.WebApp;
-  const onClose = () => {
-    tg.close();
-  };
+  const { user, onClose } = useTelegram();
+
   return (
     <header className={`header`}>
       <Button onClick={onClose}>Закрыть</Button>
-      <span className="username">{tg.initDataUnsafe?.user?.username}</span>
+      <span className="username">{user}</span>
     </header>
   );
 };
